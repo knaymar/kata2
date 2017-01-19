80611ac8afbaa7727c00b555c30abaea9873881a
aetsmtl <arnold.fotsing.nde@esigetel.net>
2017-01-19 18:09:21 +0100

    first functionnal version of application

---
+++ fizzApp/src/main/java/com/aetsmtl/fizzbApp/Buzzer.java
@@ -0,0 +1,41 @@
+package com.aetsmtl.fizzbApp;
+
+public class Buzzer {
+	
+	private int inputNumber ;
+	
+	private static final String FIZZ = "fizz";
+	private static final String BUZZ = "buzz";
+	private static final String BUZZFIZZ = "fizzBuzz";
+	
+	public Buzzer() 
+	{
+		super();
+	}
+
+	public Buzzer(int value) 
+	{
+		this.inputNumber = value;
+	}
+	
+	public int getInputNumber() {
+		return inputNumber;
+	}
+
+	public void setInputNumber(int inputNumber) {
+		this.inputNumber = inputNumber;
+	}
+
+	public String checkFizzBuzz() 
+	{
+		if (inputNumber % 3 == 0 && inputNumber % 5 == 0)
+			return BUZZFIZZ;
+		else if (inputNumber % 5 == 0)
+			return BUZZ;
+		else if (inputNumber % 3 == 0)
+			return FIZZ;
+		else 
+			return String.valueOf(inputNumber);	
+	}
+	
+}
